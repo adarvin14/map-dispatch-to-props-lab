b9171d328a052afa4fc13a816e94c87841052ab6
adarvin14 <amdarvin14@gmail.com>
2021-04-05 17:22:39 -0400

    commit tree 126cb18014e02151d3c9ce7917873a543c7b2802

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,9 +1,11 @@
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Restaurants extends Component {
 
   render() {
+    console.log(this.props.restaurant)
 
     let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}>{restaurant.name}</li>);
 
